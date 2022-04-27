6093c0f541f99b5711753ca6bf5950ecab83b8d1
Thalles Eduardo Oliveira Bueno <thalles_bueno@terceiros.sicredi.com.br>
2022-04-27 20:19:42 -0300

    button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  height: 48px ;
+  padding: 8px 20px;
+  
+  color: ${({ theme }) => theme.colors.backgroundColorLight};
+  font-size: 20px;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  
+  background-color: ${({ theme }) => theme.colors.mainColor};
+  cursor: pointer;
+
+  border-radius: 12px;
+  border: none;
+  box-shadow: none;
+
+  &:hover {
+    box-shadow: 0px 2px 10px rgba(240, 125, 0, 0.3);
+  }
+
+  &:active {
+    background-color: ${({ theme }) => theme.colors.mainColorDarker};
+  }
+`;
+
+interface props {
+  children: ReactNode,
+}
+
+const Button: React.FC<props> = ({ children }) => {
+  return (
+    <StyledButton>{children}</StyledButton>
+  );
+};
+
+export default Button;
