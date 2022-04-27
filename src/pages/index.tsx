a1b7fcb4bbfb30abd0fa36dda4d3bff15f88306a
Thalles Eduardo Oliveira Bueno <thalles_bueno@terceiros.sicredi.com.br>
2022-04-27 16:08:42 -0300

    homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import Link from 'next/link';
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Welcome to the Trivia Challange!</h1>
+      <p>You will be presented with 10 True or False questions.</p>
+      <p>Can you score 100%?</p>
+      <Link href="/quiz">
+        <a>Begin quiz</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
