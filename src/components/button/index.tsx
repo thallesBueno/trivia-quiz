4831a43adfabe2f9abcd5ad720be763ec7de746d
Thalles Eduardo Oliveira Bueno <thalles_bueno@terceiros.sicredi.com.br>
2022-04-28 10:39:42 -0300

    button variant

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,38 +1,20 @@
 import { ReactNode } from 'react';
-import styled from 'styled-components';
+import { StyledButton } from './style';
 
-const StyledButton = styled.button`
-  height: 48px ;
-  padding: 8px 20px;
-  
-  color: ${({ theme }) => theme.colors.backgroundColorLight};
-  font-size: 20px;
-  text-transform: uppercase;
-  letter-spacing: 1px;
-  
-  background-color: ${({ theme }) => theme.colors.mainColor};
-  cursor: pointer;
-
-  border-radius: 12px;
-  border: none;
-  box-shadow: none;
-
-  &:hover {
-    box-shadow: 0px 2px 10px rgba(240, 125, 0, 0.3);
-  }
-
-  &:active {
-    background-color: ${({ theme }) => theme.colors.mainColorDarker};
-  }
-`;
-
+enum ButtonVariant {
+  primary,
+  success,
+  error,
+}
 interface props {
   children: ReactNode,
-  onClick: () => {};
+  onClick: () => void;
+  variant?: ButtonVariant,
 }
 
-const Button: React.FC<props> = ({ children, onClick }) =>  (
-  <StyledButton onClick={onClick}>{children}</StyledButton>
+const Button: React.FC<props> = ({ children, onClick, variant = ButtonVariant.primary }) =>  (
+  <StyledButton onClick={onClick} variant={variant}>{children}</StyledButton>
 );
 
+export { ButtonVariant };
 export default Button;
---
+++ src/components/button/style.ts
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { ButtonVariant } from ".";
+
+const StyledButton = styled.button`
+height: 48px ;
+padding: 8px 20px;
+
+color: ${({ theme }) => theme.colors.white};
+font-size: 20px;
+text-transform: uppercase;
+letter-spacing: 1px;
+
+background-color: ${({ theme, variant }) => {
+  if (variant === ButtonVariant.success) {
+    return theme.colors.green;
+  }
+
+  if (variant === ButtonVariant.error) {
+    return theme.colors.red;
+  }
+  return theme.colors.mainColor;
+}};
+
+cursor: pointer;
+
+border-radius: 12px;
+border: none;
+box-shadow: none;
+
+&:hover {
+  box-shadow: 0px 2px 10px rgba(240, 125, 0, 0.3);
+}
+
+&:active {
+  background-color: ${({ theme, variant }) => {
+  if (variant === ButtonVariant.success) {
+    return theme.colors.darkGreen;
+  }
+
+  if (variant === ButtonVariant.error) {
+    return theme.colors.darkRed;
+  }
+  return theme.colors.mainColorDarkers;
+}};
+}
+`;
+
+export { StyledButton };
